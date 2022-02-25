70c380ba19271c8519966a3e503561b9a62943a1
Benja <benjarc99@gmail.com>
2022-02-25 13:19:34 -0300

    segunda subida

--- header.js
+++ header.js
@@ -2,12 +2,12 @@ const d = document,
   w = window;
 
 const $navHeader = d.querySelector(".nav-header");
-console.log($navHeader);
+
+$navHeader.classList.remove("scroll");
 
 d.addEventListener("scroll", () => {
   let y = w.scrollY;
 
-  console.log(y);
   if (y === 0) {
     $navHeader.classList.remove("scroll");
   }
